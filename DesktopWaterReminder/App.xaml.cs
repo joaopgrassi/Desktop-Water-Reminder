d0e51ebf90a367d280cdd6816e452abf26819303
Joao Grassi <joaop.grassi@gmail.com>
2016-01-10 15:22:40 -0200

    initial project creation

---
+++ DesktopWaterReminder/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Hardcodet.Wpf.TaskbarNotification;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DesktopWaterReminder
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private TaskbarIcon notifyIcon;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            notifyIcon = (TaskbarIcon)FindResource("NotifyIcon");
+        }
+        protected override void OnExit(ExitEventArgs e)
+        {
+            notifyIcon.Dispose();
+            base.OnExit(e);
+        }
+    }
+}
